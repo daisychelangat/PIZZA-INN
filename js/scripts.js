93fb2a84649cfe98df39fb96a8af5019ea581e47
daisychelangat <daisyrop2000@gmail.com>
2021-01-11 17:53:56 +0300

    updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,46 @@ function toppingss() {
     }
     return parseInt(mushroom);
 }
+
+function calctotalPrice(e) {
+    event.preventDefault();
+    var totalPrice = (getCostPerSize() + getCostPerCrust() + toppingss()) * (getQuantity());
+
+        console.log(totalPrice);
+    alert("Dear customer you have ordered an amount of " + getQuantity() +  " pizzas and the amount you owe us is " + totalPrice +"." )
+
+}
+
+
+
+// option for delivery//
+
+
+
+$(document).ready(function () {
+    $("#home").submit(function () {
+
+        var name = $("input#name").val();
+        var number = $("input#number").val();
+        var location = $("input#location").val();
+
+        alert("Hey!" + name + ". Tam Tam Pizza has received your order and it will soon be delivered to you at " +  location + ".Delivery will cost an extra sh 200 payable on delivery.We are glad to serve you.Thank you for choosing us.");
+
+        
+    });
+
+});
+// cart//
+$(document).ready(function () {
+    $("#pizzainfo").submit(function () {
+
+        var type = $("optiont#type").val();
+        var quantity = $("input#number").val();
+        var crust = $("optiont#crust").val();
+        var topping = $("option#toppingone").val();
+        append("This is the summary of your order" + type + crust + yy)
+
+
+    });
+
+});
